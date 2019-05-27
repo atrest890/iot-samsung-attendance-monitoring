940a4fe8e90cbf75334b7a65f5b2004b642acabe
Daniil159x <daniil.ru-m@mail.ru>
2019-05-28 00:06:24 +0700

    nfc main loop added

--- nfc_script/nfc.py
+++ nfc_script/nfc.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import time
+
 from smartcard.scard import *
 import smartcard.util
 
@@ -45,7 +47,45 @@ hres, hcard, dwActiveProtocol = SCardConnect(hcontext, reader,
                             SCARD_SHARE_SHARED, SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1)
 pcsc_utils.check(hres, 'connect')
 
+# отключаем биб
 hres, response = SCardTransmit(hcard, dwActiveProtocol, DISABLE_STD_BUZZER)
 pcsc_utils.check(hres, 'disable standart buzzer')
+
+# и 2 раза бибикним
+hres, response = SCardTransmit(hcard, dwActiveProtocol, BUZZING)
+pcsc_utils.check(hres, 'init buzzing 1')
+
+time.sleep(0.5)
+
+hres, response = SCardTransmit(hcard, dwActiveProtocol, BUZZING)
+pcsc_utils.check(hres, 'init buzzing 1')
+
+# ожидаем освобождение ридера
+while pcsc_utils.insertedCardState(readerstates[0]):
+    print("Leave a tag from the device")
+    timeout_calibration = 30 # 30 секунд?
+    hres, readerstates = SCardGetStatusChange(hcontext, timeout_calibration, readerstates)
+    pcsc_utils.check(hres, 'get status change')
+
+print("Calibration is finished!")
+
+while True:
+    # TODO: а что с исключениями?
+    # ждём карту
+    hres, readerstates = SCardGetStatusChange(hcontext, INFINITE, readerstates)
+    pcsc_utils.check(hres, 'get status change')
+    pcsc_utils.printState(readerstates[0])
+
+    # получаем tag
+    hres, response = SCardTransmit(hcard, dwActiveProtocol, APDU)
+    pcsc_utils.check(hres, 'get apdu')
+    print("responce:", smartcard.util.toHexString(response, smartcard.util.HEX))
+
+    # TODO: а что если ошибочка?
+
+    # сообщаяем звуком
+    hres, response = SCardTransmit(hcard, dwActiveProtocol, BUZZING)
+    pcsc_utils.check(hres, 'ending buzzing')
+
 
 
