baaf80cee0c5d7951ba51ed435051b3b52adcbe6
Daniil159x <daniil.ru-m@mail.ru>
2019-05-28 21:38:47 +0700

    High level nfc and sender added

---
+++ nfc_script/nfc_highlevel.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+
+from enum import Enum
+
+from smartcard.CardType import AnyCardType
+from smartcard.CardRequest import CardRequest
+from smartcard.Exceptions import CardRequestTimeoutException
+from smartcard.scard import INFINITE
+
+import ACR122_codes
+import nfc_sender
+
+
+class State(Enum):
+    INIT = 0
+    PROCESSING = 1
+
+def waitConnect(timeout=INFINITE, cardType=AnyCardType(), newcardonly=True):
+    cardrequest = CardRequest(timeout=timeout, cardType=cardType, newcardonly=True)
+    cardservice = cardrequest.waitforcard()
+    return cardservice.connection
+
+
+
+state = State.INIT
+
+# init
+while state == State.INIT:
+    try:
+        print("Calibration Time!")
+        print("Place a tag on the device")
+        conn = waitConnect()
+        # TODO: а что по ошибкам?
+        conn.connect()
+        conn.transmit(ACR122_codes.DISABLE_STD_BUZZER)
+        conn.transmit(ACR122_codes.BUZZING)
+        conn.transmit(ACR122_codes.BUZZING)
+        conn.disconnect()
+        print("Calibration is finished!")
+        print("Leave a tag from the device")
+    except Exception as e:
+        print(e)
+
+state = State.PROCESSING
+sender = nfc_sender.NFCSender("192.168.43.9", 9999)
+
+# main loop
+while state == State.PROCESSING:
+    try:
+        conn = waitConnect(newcardonly=False)
+        conn.connect()
+        response, sw1, sw2 = conn.transmit(ACR122_codes.APDU)
+
+        if sw1 == 0x90 and sw2 == 0x00:
+            sender.Append(response)
+        else:
+            raise Exception( "Bad response on transmit[" + response + "], sw = " + str((hex(sw1), hex(sw2))) )
+
+        conn.transmit(ACR122_codes.BUZZING)
+        conn.disconnect()
+    except Exception as e:
+        print(e)
+
---
+++ nfc_script/nfc_sender.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import threading
+import time
+
+class NFCSender():
+    """
+    Задача NFCSender - отправлять на сервер nfc теги.
+    """
+    TIMER = 30 # секунды
+
+    def __init__(self, ip, port):
+        self._listToSend = []
+        # self._thread = threading.Thread(self.__send)
+        self._timer = self.__createTimer()
+        self._hasStop = False
+
+    def __createTimer(self):
+        return threading.Timer(self.TIMER, self.__send)
+
+    def __send(self):
+        localList = self._listToSend
+        self._listToSend = []
+        
+        for i in localList:
+            print("Sending", i, "...")
+            print("ERROR: Sending not impl")
+            print("Sended [", 404, ']')
+
+        self._timer = self.__createTimer()
+        if not self._hasStop:
+            self.Start()
+        
+    def Start(self):
+        self._hasStop = False
+        self._timer.start()
+
+    def Stop(self):
+        self._hasStop = True
+        self._timer.cancel()
+
+    def Append(self, nfc):
+        self._listToSend.append(nfc)
