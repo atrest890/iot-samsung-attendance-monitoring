ffb5d81ef3c2223ee8f047f247071e9cf0726460
atrest890 <swarovski991@gmail.com>
2019-06-04 02:27:18 +0700

    bootstrap new version 4.3.1, buttons, dark-theme added, makeup

--- webapp/website/views.py
+++ webapp/website/views.py
@@ -48,7 +48,6 @@ def faculties(request, faculty):
         urls[group_number] = f"/faculties/{faculty}/groups/{group_number}"
         all_groups[course].append({"number" : group_number, "url" : urls[group_number]})
             
-    print(urls)
     return render(request, 'website/faculty.html', context = { "all_groups" : all_groups })
 
 
@@ -65,11 +64,13 @@ def groups(request, faculty, group):
     StudentObjList = Student.objects.filter(group = GroupObj.id)
     students = {}
 
+    group_number = Group.objects.get(group_number = group).group_number
     for st in StudentObjList:
         fullname = f"{st.surname} {st.name} {st.patronymic}"
         students[fullname] = f"/faculties/{faculty}/groups/{group}/students/{st.id}"
 
-    return render(request, 'website/group.html', context = {"students" : students})
+    return render(request, 'website/group.html', context = {"students" : students,
+                                                            "group" : group_number})
 
 
 
@@ -77,10 +78,31 @@ def students(request, faculty, group, student_id):
     StudentObj = Student.objects.get(id = student_id)
     fullname_and_group = mu.getFullNameAndGroup(StudentObj)
 
+    #TODO: добавить за весь период
+
+    today = date.today()
+    if 'month' in request.GET:
+        month = today.month
+        year = today.year
+        start_date = datetime(year, month, 1)
+        end_date = today
+
+    elif 'year' in request.GET:
+        year = today.year
+        if datetime.today().month < 9:
+            year -= 1
+        start_date = datetime(year, 9, 1)
+        end_date = today
+
+    else:
+        iso = datetime.today().isocalendar()
+        start_date = datetime.strptime(f'{iso[0]}-{iso[1]-1}-1', '%Y-%W-%w')
+        end_date = today
+        
+
     GroupObj = Group.objects.get(group_number = group)
     GroupLessonObjList = Group_Lesson.objects.filter(group = GroupObj)
-    LessonObjList = Lesson.objects.filter(id__in = GroupLessonObjList.values("lesson"))
-
+    LessonObjList = Lesson.objects.filter(id__in = GroupLessonObjList.values("lesson"), date__range = (start_date, end_date))
     attendance = Attendance.objects.filter(student = student_id).values_list("lesson", flat = True)
  
     lessons = []
@@ -95,9 +117,16 @@ def students(request, faculty, group, student_id):
                         "aud" : l.auditorium.aud_number,
                         "build" : l.auditorium.building.build_name,
                         "status" : status})
-    
+
+    urls = {"week" : f"{student_id}?week",
+            "month" : f"{student_id}?month",
+            "year" : f"{student_id}?year"}
+
+
     return render(request, 'website/student.html', context = {"lessons" : lessons, 
-                                                              "fullname_and_group" : fullname_and_group})
+                                                              "fullname_and_group" : fullname_and_group,
+                                                              "urls" : urls})
+
 
 
 
@@ -117,8 +146,9 @@ def buildings(request, building):
         auditoriums[aud.aud_number] = [None] * (7*6)
         
         for less in LessonObjList:
-            auditoriums[aud.aud_number][datetime.weekday(less.date)*7 + less.lesson_number - 1] = {"name": less.abbreviation, 
-                                                               "url": f"{building}/auditoriums/{aud.aud_number}/date/{less.date}/index/{less.lesson_number}"}
+            auditoriums[aud.aud_number][datetime.weekday(less.date)*7 + less.lesson_number - 1] = {
+                                                                "name": less.abbreviation, 
+                                                                "url": f"{building}/auditoriums/{aud.aud_number}/date/{less.date}/index/{less.lesson_number}"}
 
     building_name = BuildObj.fullname
 
@@ -157,9 +187,4 @@ def lessons(request, building, auditorium, date, index):
 
     return render(request, 'website/lesson.html', context = {"students" : students,
                                                              "lesson" : lesson})
-
-
-
-
-
-        
+ 
